2d96596a78b5a9064916f567982b8d5b799c379d
adam-wit <adam@wit-fitness.com>
2021-03-07 19:46:08 +0000

    Added reusabled component for drop down element

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Wicket" animal="Dog" breed="German Shepard"/>
-      <Pet name="Sooty" animal="Cat" breed="Mixed"/>
-      <Pet name="Bert" animal="Bird" breed="Parrot"/>
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropDown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+
+  // const [animal, setAnimal] = useState("Dog");
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  // const [breed, setBreed] = useState("");
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+
+        <AnimalDropdown />
+        <BreedDropdown />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
