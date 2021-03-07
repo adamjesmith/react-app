9ee76d9c8628d9e3e11e421dec0e7ddb689f762d
adam-wit <adam@wit-fitness.com>
2021-03-07 10:22:39 +0000

    Added JS files and animal breeds

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Wicket",
+      animal: "Dog",
+      breed: "German Shepard",
+    }),
+    React.createElement(Pet, {
+      name: "Sooty",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Bert",
+      animal: "Bird",
+      breed: "Parrot",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
