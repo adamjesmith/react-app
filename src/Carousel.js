0eab89e2a677124ea968c8f563d16367ca87f1ad
adam-wit <adam@wit-fitness.com>
2021-03-10 19:02:23 +0000

    Added error boundaries

--- src/Carousel.js
+++ src/Carousel.js
@@ -6,7 +6,7 @@ class Carousel extends React.Component {
     active: 0
   };
   static getDerivedStateFromProps({ media }) {
-    let photos = ['http://placecorgi.com/600/600'];
+    let photos = ["http://placecorgi.com/600/600"];
 
     if (media.length) {
       photos = media.map(({ large }) => large);
@@ -17,29 +17,29 @@ class Carousel extends React.Component {
   handleIndexClick = event => {
     this.setState({
       active: +event.target.dataset.index
-    })
-  }
+    });
+  };
   render() {
-    const { photos, active } =  this.state;
+    const { photos, active } = this.state;
 
     return (
       <div className="carousel">
-        <img src={photos[active]} alt="animal"/>
-        <div className="carousel-smaller">{
-          photos.map((photo, index) => (
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
             // eslint-disable-next-line
-            <img 
+            <img
               key={photo}
               onClick={this.handleIndexClick}
               data-index={index}
               src={photo}
-              className={index === active ? "active": ""}
+              className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
-          ))
-        }</div>
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
 
